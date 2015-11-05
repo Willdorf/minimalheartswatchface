cb4390e57325987747384d655dadfb611933e610
Willdorf <william.g.armstrong@gmail.com>
2015-11-05 12:06:26 -0500

    update where configuration page is currently hosted

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -3,7 +3,7 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-	var url = 'http://e0acbef6.ngrok.io';
+	var url = 'http://armstrongwill.com/pebbleconfigurationpages/minimalhearts/index.html';
 
 	console.log('Showing configuration page: ' + url);
 
