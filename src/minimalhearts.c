1cbea97fca7d109d3f82e91d1a69c401816c0b7a
Willdorf <william.g.armstrong@gmail.com>
2015-11-08 17:25:26 -0500

    added bluetooth connectivity indicator

--- src/minimalhearts.c
+++ src/minimalhearts.c
@@ -10,6 +10,9 @@ static TextLayer *s_time_layer;
 static GColor background_color;
 static GColor heart_color;
 
+static BitmapLayer *s_bt_icon_layer;
+static GBitmap *s_bt_icon_bitmap;
+
 static uint8_t s_hour;
 static uint8_t s_min;
 
@@ -21,6 +24,17 @@ static const GPathInfo HEART_INFO = {
 static GPath *heart = NULL;
 
 static uint8_t s_sec;
+
+
+static void bluetooth_callback(bool connected) {
+	// show icon if disconnected
+	layer_set_hidden(bitmap_layer_get_layer(s_bt_icon_layer), connected);
+
+	if (!connected) {
+		//issue a vibrating alert
+		vibes_double_pulse();
+	}
+}
 
 static void update_time(struct tm *tick_time) {
 	s_hour = tick_time->tm_hour;
@@ -172,36 +186,51 @@ static void inbox_received_handler(DictionaryIterator *iter, void *context) {
 }
 
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+	Layer *window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_bounds(window_layer);
 
-  s_layer = layer_create(layer_get_bounds(window_get_root_layer(window)));
-  layer_add_child(window_get_root_layer(window), s_layer);
-  layer_set_update_proc(s_layer, draw_watchface);
+	s_layer = layer_create(layer_get_bounds(window_get_root_layer(window)));
+	layer_add_child(window_get_root_layer(window), s_layer);
+	layer_set_update_proc(s_layer, draw_watchface);
 
-  //text layer for displaying the minutes
-  s_time_layer = text_layer_create(GRect(0,0,24,24));
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorBlack);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
+	//text layer for displaying the minutes
+	s_time_layer = text_layer_create(GRect(0,0,24,24));
+	text_layer_set_background_color(s_time_layer, GColorClear);
+	text_layer_set_text_color(s_time_layer, GColorBlack);
+	text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+	text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+	layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_time_layer));
 
-  setup_paths();
+	setup_paths();
 
-  if (persist_read_int(KEY_BACKGROUND_COLOR)) {
-	  set_background_and_text_color(persist_read_int(KEY_BACKGROUND_COLOR));
-  } else {
-	  background_color = GColorVividCerulean;
-	  window_set_background_color(window, background_color);
-	  text_layer_set_text_color(s_time_layer, gcolor_legible_over(background_color));
-  }
+	if (persist_read_int(KEY_BACKGROUND_COLOR)) {
+		set_background_and_text_color(persist_read_int(KEY_BACKGROUND_COLOR));
+	} else {
+		background_color = GColorVividCerulean;
+		window_set_background_color(window, background_color);
+		text_layer_set_text_color(s_time_layer, gcolor_legible_over(background_color));
+	}
 
-  if (persist_read_int(KEY_HEART_COLOR)) {
-	  heart_color = GColorFromHEX(persist_read_int(KEY_HEART_COLOR));
-  } else {
-	  heart_color = GColorRed;
-  }
+	if (persist_read_int(KEY_HEART_COLOR)) {
+		heart_color = GColorFromHEX(persist_read_int(KEY_HEART_COLOR));
+	} else {
+		heart_color = GColorRed;
+	}
+
+	// Create the Bluetooth icon GBitmap
+	s_bt_icon_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BT_ICON);
+
+	// Create the BitmapLayer to display the GBitmap
+	s_bt_icon_layer = bitmap_layer_create(GRect(0, 0, 30, 30));
+	bitmap_layer_set_bitmap(s_bt_icon_layer, s_bt_icon_bitmap);
+	layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_bt_icon_layer));
+
+	//show the correct state of the bluetooth connection from the start
+#ifdef PBL_SDK_2
+	bluetooth_callback(bluetooth_connection_service_peek());
+#elif PBL_SDK_3
+	bluetooth_callback(connection_service_peek_pebble_app_connection());
+#endif
 
 }
 
@@ -210,38 +239,51 @@ static void window_unload(Window *window) {
 
 	//destroy the main layer
 	layer_destroy(s_layer);
+
+	//destroy the bluetooth stuffs
+	gbitmap_destroy(s_bt_icon_bitmap);
+	bitmap_layer_destroy(s_bt_icon_layer);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+	window = window_create();
+	window_set_window_handlers(window, (WindowHandlers) {
+			.load = window_load,
+			.unload = window_unload,
+			});
+	const bool animated = true;
+	window_stack_push(window, animated);
 
-  //Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+	//Register with TickTimerService
+	tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
 
-  app_message_register_inbox_received(inbox_received_handler);
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+	app_message_register_inbox_received(inbox_received_handler);
+	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 
-  //display the time right away
-  time_t start_time = time(NULL);
-  update_time(localtime(&start_time));
+	//Register for Bluetooth connections updates
+#ifdef PBL_SDK_2
+	bluetooth_connection_service_subscribe(bluetooth_callback);
+#elif PBL_SDK_3
+	connection_service_subscribe((ConnectionHandlers) {
+			.pebble_app_connection_handler = bluetooth_callback
+			});
+#endif
+
+	//display the time right away
+	time_t start_time = time(NULL);
+	update_time(localtime(&start_time));
 }
 
 static void deinit(void) {
-  window_destroy(window);
-  tick_timer_service_unsubscribe();
+	window_destroy(window);
+	tick_timer_service_unsubscribe();
 }
 
 int main(void) {
-  init();
+	init();
 
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+	APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
 
-  app_event_loop();
-  deinit();
+	app_event_loop();
+	deinit();
 }
